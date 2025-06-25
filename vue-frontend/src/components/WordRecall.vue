bf935fe547f5fe3e1e33cf5c4c25dbce73bdbd77
Andrew Nam <dan6ra@virginia.edu>
2025-06-24 23:05:23 -0700

    Added begin button, speech synthesis with Web Speech API

--- vue-frontend/src/components/WordRecall.vue
+++ vue-frontend/src/components/WordRecall.vue
@@ -16,14 +16,13 @@ const isGuessesFinished = ref(currentWords.value.length == props.wordCount)
 const SERVER = new URL('http://127.0.0.1:8000/random-words/')
 var retries = 0
 const retryTotal = 5
+const synth = window.speechSynthesis
 
 onBeforeMount(() => {
     populateWordBank()
 })
 
 onMounted(() => {
-    getNewWords()
-    console.log(wordGuesses)
 })
 
 async function populateWordBank() {
@@ -32,7 +31,8 @@ async function populateWordBank() {
         wordBank.value = result as string[]
         console.log(wordBank.value)
         console.log(wordBank.value.length)
-        retries = 0
+        retries = 0;
+        (document.getElementById("begin-button") as HTMLElement).style.visibility = "visible";
     } catch (e) {
         console.error((e as Error).message)
         if (retries < retryTotal) {
@@ -55,7 +55,7 @@ function getNewWords() {
 }
 
 function playAgain() {
-    console.log("play again")
+    generateAudio()
 }
 
 function reset() {
@@ -92,30 +92,63 @@ function testWord() {
 function skip() {
     reset()
     populateWords()
+    generateAudio()
+}
+
+function begin() {
+    populateWords();
+    var beginButton = document.getElementById("begin-button");
+    (beginButton as HTMLElement).style.display="none";
+    var gameArea = document.getElementsByClassName("gameArea");
+    (gameArea.item(0) as HTMLElement).style.visibility="visible";
+    
+    generateAudio()
+}
+
+function generateAudio() {
+    console.log("Generating audio")
+    var utterance = wordContainerList.value.join(" ")
+    console.log(utterance)
+    const utterThis = new SpeechSynthesisUtterance(utterance)
+    utterThis.pitch = 1
+    utterThis.rate = .25
+    const voices = speechSynthesis.getVoices();
+    for (const voice of voices) {
+        if (voice.name === "Samantha") {
+        utterThis.voice = voice;
+        }
+    }
+    synth.speak(utterThis)
 }
 
 </script>
 
 <template>
-    <div class="wordsContainer">
-        <WordContainer 
-            v-for="(word, index) in wordContainerList"
-            :isComplete="isGuessesFinished"
-            :word="word"
-            :wordGuess="wordGuesses[index]"
-            :key="index"
-        ></WordContainer>
+    <button @click="begin" id="begin-button" visibility="hidden">Begin</button>
+    <div class="gameArea">
+        <div class="wordsContainer">
+            <WordContainer 
+                v-for="(word, index) in wordContainerList"
+                :isComplete="isGuessesFinished"
+                :word="word"
+                :wordGuess="wordGuesses[index]"
+                :key="index"
+            ></WordContainer>
+        </div>
+        <button @click="playAgain" class="buttons">Play Again</button>
+        <button @click="reset" class="buttons">Reset</button>
+        <button @click="skip" class="buttons">Skip</button>
+        <button @click="testWord" class="buttons">Test</button>
     </div>
-    <button @click="playAgain">Play Again</button>
-    <button @click="reset">Reset</button>
-    <button @click="skip">Skip</button>
-    <button @click="testWord">Test</button>
-
 </template>
 
 <style>
 .wordsContainer {
     display: flex;
+}
+
+.gameArea {
+    visibility: hidden;
 }
 
 ol {
